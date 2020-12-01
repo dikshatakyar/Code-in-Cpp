42ffca551c67dc5fbce9eee810618501daf08585
dikshatakyar <75247382+dikshatakyar@users.noreply.github.com>
2020-12-01 17:54:33 +0530

    Add files via upload

---
+++ for.cpp.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//to display multiplication table upto 10//
+#include <iostream>
+
+using namespace std;
+int main(){
+
+int d;
+cout<<"enter a positive value"<<endl;
+cin>>d;
+
+for(int v=1;v<=10;v++) {
+    cout<<v*d<<endl;
+    
+}
+
+return 0;
+}
