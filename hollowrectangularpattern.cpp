38f9dde86a725aad729bb6f25ece77e3bcae5c55
Diksha <75247382+dikshatakyar@users.noreply.github.com>
2020-12-10 10:44:30 +0530

    Add files via upload

---
+++ hollowrectangularpattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+ int r,c;
+ cout<<"enter the number of rows and coloumns respectively"<<endl;
+ cin>>r>>c;
+ 
+ for(int i=1;i<=r;++i) {
+     for(int j=1;j<=c;++j) {
+         if(i==1 || i==r) { 
+         cout<<"*";
+         }
+         else if (j==1 || j==c){
+             cout<<"*";
+         }
+         else
+         cout<<" ";
+     }
+     cout<<endl;
+ }
+    
+    return 0;
+    
+}
