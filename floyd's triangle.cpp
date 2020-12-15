e811928b6e4736fc460deacaa3166e1bccc1a960
Diksha <75247382+dikshatakyar@users.noreply.github.com>
2020-12-15 09:20:17 +0530

    Add files via upload

---
+++ floyd's triangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+int i,j,n,count=1;
+cout<<"number of rows to print"<<endl;
+cin>>n;
+
+for (i=1;i<=n;++i) {
+    for(j=1;j<=i;++j) {
+    cout<<count;
+    count++;
+    }
+    cout<<endl;
+}
+    return 0;
+    
+}
