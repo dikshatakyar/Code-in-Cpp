a01fcd945df38634b0a926c93f48d5f53258f677
Diksha <75247382+dikshatakyar@users.noreply.github.com>
2021-01-03 11:01:56 +0530

    Add files via upload

---
+++ seq3.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cout<<"print till"<<endl;
+    cin>>n;
+
+    for(int i=1;i<=n;i++) {
+    if(i%3==1)
+    cout<<i<<" ";
+    else
+    {
+        continue;
+    }
+    
+    }
+    return 0;
+}
