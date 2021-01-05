891e59936b469f4cee3ddb5a3db579fc3656640e
Diksha <75247382+dikshatakyar@users.noreply.github.com>
2021-01-05 21:05:06 +0530

    Add files via upload

---
+++ reverseanumber.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,rev=0,rem; 
+    cout<<"enter a number"<<endl;
+    cin>>n;
+    while(n>0) {
+        rem=n%10;
+        rev=(rev*10) +  rem;
+        n=n/10;
+
+    }
+    cout<<rev;
+return 0;
+}
