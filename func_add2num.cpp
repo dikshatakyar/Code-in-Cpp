e238e1e0e9b608b0a9d88ddc8e4f869dac00aac0
Diksha <75247382+dikshatakyar@users.noreply.github.com>
2021-02-05 22:18:06 +0530

    Add files via upload

---
+++ func_add2num.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(int n1,int n2) {
+  int  sum=n1+n2;
+    return sum;
+}
+
+int main() {
+    int a,b;
+    cout<<"enter two numbers"<<endl;
+    cin>>a>>b;
+
+    cout<<add(a,b)<<endl;
+
+ return 0;
+
+
+}
+
