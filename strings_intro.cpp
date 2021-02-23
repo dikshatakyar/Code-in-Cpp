57dbe24030db696623798b49a9b21a02545759bb
Diksha <75247382+dikshatakyar@users.noreply.github.com>
2021-02-23 10:06:09 +0530

    Add files via upload

---
+++ strings_intro.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // string s;
+    // cin>>s;
+    // cout<<s;
+
+
+// string s(10,'K');
+// cout<<s<<endl;
+
+
+// string st="HelloThere \n";
+// cout<<st<<endl;
+
+string s;
+getline(cin,s);
+cout<<s<<endl;
+    return 0;
+
+}
