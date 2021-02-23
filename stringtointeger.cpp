cd3544be3b3c3c4106e963b33d45259dbb0f738f
Diksha <75247382+dikshatakyar@users.noreply.github.com>
2021-02-23 10:07:41 +0530

    Add files via upload

---
+++ stringtointeger.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s="790";
+    int x= stoi(s);
+    cout<<x<<"\n"<<x+10<<endl<<s<<endl;
+
+//INTEGER TO STRING
+int y=55;
+cout<<to_string(y)+"2"<<endl;
+
+    return 0;
+}
