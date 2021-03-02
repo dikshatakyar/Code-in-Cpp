9e028b460b84a8d94b49bea1d4bcbbdb1314b045
Diksha <75247382+dikshatakyar@users.noreply.github.com>
2021-03-03 00:25:09 +0530

    Add files via upload

---
+++ break.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     
+     for(int i=1;i<=15;++i) {
+         if(i==10){
+             break;
+         }
+         cout<<i<<endl;
+     }
+     return 0;
+}
+    
