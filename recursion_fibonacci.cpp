912afac5c005c5dbc0ec923bf95a5ad8d262fc52
Diksha <75247382+dikshatakyar@users.noreply.github.com>
2021-04-11 22:22:33 +0530

    Update recursion_fibonacci.cpp

--- recursion_fibonacci.cpp
+++ recursion_fibonacci.cpp
@@ -20,3 +20,22 @@ int main() {
 
 
 //this codes prints the fibonacci series:
+#include <iostream>
+
+using namespace std;
+
+int fib(int n){
+    if(n==0 || n==1)
+    return n;
+    else
+    return fib(n-1)+fib(n-2);//fib(1)+fib(0)=1--fib(2)+fib(1)=1+1=2--fib(3)+fib(2)=2+1=3
+}
+
+int main(){
+    int n;//5
+    cout<<"print till\n";
+    cin>>n;
+    for(int i=0;i<n;++i){//0 1 2 3 4
+        cout<<fib(i)<<" ";//0 1 1 2 3
+        }
+}
